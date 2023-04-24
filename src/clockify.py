1240f6ba027ce0d9cc4b61806081c6033c522d02
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2023-04-23 21:31:59 -0400

    wip

---
+++ src/clockify.py
@@ -0,0 +1,52 @@
+import json
+import logging
+from datetime import datetime
+
+import requests
+
+from config import settings
+
+
+HEADERS = {"X-Api-Key": settings.clockify.api_key}
+
+
+def get_timesheet_report(start_date, end_date):
+    logging.info("Getting timesheet report...")
+    start_date = datetime.fromisoformat(start_date).strftime(
+        settings.clockify.datetime_fmt
+    )
+    end_date = datetime.fromisoformat(end_date).strftime(settings.clockify.datetime_fmt)
+    logging.info(f"Getting timesheet report from {start_date} to {end_date}...")
+
+    # Set up the API request
+    url = f"https://reports.api.clockify.me/v1/workspaces/{settings.clockify.workspace_id}/reports/detailed"
+    payload = {
+        "dateRangeStart": start_date,
+        "dateRangeEnd": end_date,
+        "dateFormat": "YYYY-MM-DD",
+        "detailedFilter": {"groups": ["PROJECT", "TASK"]},
+        "exportType": "JSON",
+    }
+
+    # Send the API request and handle the response
+    response = requests.post(url, headers=HEADERS, json=payload)
+    response.raise_for_status()
+
+    # extract timeentries
+    return response.json()["timeentries"]
+
+
+def get_user_info():
+    logging.info("Getting user info...")
+    url = "https://api.clockify.me/api/v1/user"
+    response = requests.get(
+        url,
+        headers=HEADERS,
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def get_user_id():
+    logging.info("Getting user ID...")
+    return get_user_info()["id"]
--- src/tasks.py
+++ src/tasks.py
@@ -1,58 +1,70 @@
-import random
-from collections import defaultdict
+import json
+import logging
+import math
+from datetime import date
+from pathlib import Path
 
 import luigi
+import pandas as pd
+
+import clockify
+from config import settings
+
+WORKING_DIR = Path(settings.data.working_dir)
 
 
-class Streams(luigi.Task):
-    """
-    Faked version right now, just generates bogus data.
-    """
-
-    date = luigi.DateParameter()
-
-    def run(self):
-        """
-        Generates bogus data and writes it into the :py:meth:`~.Streams.output` target.
-        """
-        with self.output().open("w") as output:
-            for _ in range(1000):
-                output.write(
-                    "{} {} {}\n".format(
-                        random.randint(0, 999),
-                        random.randint(0, 999),
-                        random.randint(0, 999),
-                    )
-                )
-
-    def output(self):
-        """
-        Returns the target output for this task.
-        In this case, a successful execution of this task will create a file in the local file system.
-        :return: the target output for this task.
-        :rtype: object (:py:class:`luigi.target.Target`)
-        """
-        return luigi.LocalTarget(self.date.strftime("data/streams_%Y_%m_%d_faked.tsv"))
-
-
-class CollectTimesheet(luigi.Task):
+class ClockifyTimeEntries(luigi.Task):
     date_interval = luigi.DateIntervalParameter()
 
     def output(self):
-        return luigi.LocalTarget(f"data/artist_streams_{self.date_interval}.tsv")
-
-    def requires(self):
-        return [Streams(date) for date in self.date_interval]
+        # fname = f"{settings.data.target_fname}_{self.date_interval.start}-{self.date_interval.end}.json"
+        fname = f"{settings.data.target_fname}.json"
+        path = WORKING_DIR / fname
+        logging.info(f"Saving output to {path}")
+        return luigi.LocalTarget(path)
 
     def run(self):
-        artist_count = defaultdict(int)
+        json_data = clockify.get_timesheet_report(
+            start_date=str(self.date_interval.start),
+            end_date=str(self.date_interval.end),
+        )
 
-        for input in self.input():
-            with input.open("r") as in_file:
-                for line in in_file:
-                    timestamp, artist, track = line.strip().split()
-                    artist_count[artist] += 1
+        with self.output().open("w") as outfile:
+            json.dump(json_data, outfile)
 
-        with self.output().open("w") as out_file:
-            for artist, count in artist_count.iteritems():
-                print >> out_file, artist, count
+
+class TimeEntriesToDataFrame(luigi.Task):
+    time_entries = luigi.Parameter()
+
+    def output(self):
+        return luigi.LocalTarget(self.time_entries.replace(".json", ".csv"))
+
+    def run(self):
+        # read the JSON file into a pandas dataframe
+        df = pd.read_json(self.time_entries)
+
+        # save the dataframe to a CSV file
+        df.to_csv(self.output().path, index=False)
+
+        # convert to pandas dataframe
+        df = pd.json_normalize(self.time_entries)
+        df = df.filter(
+            items=[
+                "description",
+                "userName",
+                "projectName",
+                "clientName",
+                "timeInterval.start",
+                "timeInterval.duration",
+            ]
+        )
+
+        # convert duration seconds to hours
+        df["timeInterval.duration"] = df["timeInterval.duration"] / (60 * 60)
+
+        # round up to the nearest tenth of an hour
+        df["timeInterval.rounded"] = df["timeInterval.duration"].apply(
+            lambda x: math.ceil(x * 10) / 10
+        )
+
+        df.to_csv(self.output(), index=False)
